f07e3792a67fb2526342e70a5a8fdc07547ab336
chaehyeon7 <81363193+chaehyeon7@users.noreply.github.com>
2021-08-18 15:18:03 +0900

    Eidt MainActivity.java(메뉴 안에 호출) Commit

--- app/src/main/java/kr/hs/emirim/chaehyeon/study_activity6/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/chaehyeon/study_activity6/MainActivity.java
@@ -1,9 +1,12 @@
 package kr.hs.emirim.chaehyeon.study_activity6;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +22,23 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.my_menu, menu);
         return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.add:
+                //makeText( Context, title, 호출시간) ;
+                Toast.makeText( MainActivity.this, "add button click",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.edit:
+                Toast.makeText( MainActivity.this, "edit button click",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.email:
+                Toast.makeText( MainActivity.this, "email button click",Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
