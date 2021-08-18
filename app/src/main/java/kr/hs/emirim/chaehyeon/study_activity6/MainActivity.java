199ba92a892a76a891289f51a23420e4780634d8
chaehyeon7 <81363193+chaehyeon7@users.noreply.github.com>
2021-08-18 15:14:42 +0900

    Eidt 메뉴 호출 Commit

--- app/src/main/java/kr/hs/emirim/chaehyeon/study_activity6/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/chaehyeon/study_activity6/MainActivity.java
@@ -3,6 +3,7 @@ package kr.hs.emirim.chaehyeon.study_activity6;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //엑티비티 타이틀바에 메뉴 호출 기능을 추가
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+        return true;
+
     }
 }
